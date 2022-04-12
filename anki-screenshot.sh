fc6226f0937f79659fcc31d9c7253ec790bcaf82
B0sh <b0shbbl@hotmail.com>
2022-04-11 21:29:45 -0500

    Merge downloads folder & screenshot commands based on file modifation dates

--- anki-screenshot.sh
+++ anki-screenshot.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
+# python3 must be installed
+
 cd ~/Library/Application\ Support/Anki2/User\ 1/collection.media
 
 echo "### B0sh's script: send screenshot to recent anki card ###"
 echo ""
 
-# option for youtube screenshot
-if [ "$1" = "downloads" ]; then
+
+noteID=$(curl -s -d '{
+    "action": "findNotes",
+    "version": 6,
+    "params": {
+        "query": "added:1"
+    }
+}' -H 'Content-Type: application/json' http://127.0.0.1:8765 | python3 -c '
+import json,sys
+obj=json.load(sys.stdin)
+if obj["result"]:
+    print (max(obj["result"]))
+else:
+    print ("None")')
+
+if [ "$noteID" = "None" ]; then
+    echo "No new note found"
+    exit
+fi
+
+echo "Latest Anki Note ID: $noteID"
+
+# YouTube screenshot extension support:
+#   If there is a png file downloaded in the last 3 minutes, use that
+#   instead of taking a screenshot
+recentPngFiles="$(find ~/Downloads/*.png -cmin -3)"
+if [ -z "$recentPngFiles" ]; then
+    imgsrc="Screenshot Mac $(date +"%Y-%m-%d %H-%M-%S").png"
+    echo $imgsrc 
+    screencapture -i "$imgsrc"
+else 
     imgsrc="$(ls -t ~/Downloads | head -n1)"
     extension="${imgsrc: -3}"
     if [ "$extension" = "png" ]; then
@@ -18,10 +49,6 @@ if [ "$1" = "downloads" ]; then
     else
         echo "No image in downloads"
     fi
-else
-    imgsrc="Screenshot Mac $(date +"%Y-%m-%d %H-%M-%S").png"
-    echo $imgsrc 
-    screencapture -i "$imgsrc"
 fi
 
 # Add screenshot to last created anki card with AnkiConnect addon
@@ -36,18 +63,6 @@ if [ -f "$imgsrc" ]; then
             "query": "\"nid:'1630873192505'\""
         }
     }' -H 'Content-Type: application/json' http://127.0.0.1:8765)
-
-    noteID=$(curl -s -d '{
-        "action": "findNotes",
-        "version": 6,
-        "params": {
-            "query": "added:1"
-        }
-    }' -H 'Content-Type: application/json' http://127.0.0.1:8765 |
-    python3 -c 'import json,sys;obj=json.load(sys.stdin);print (max(obj["result"]))')
-
-    echo "Got latest note ID: $noteID"
-
     
     updateNoteFields=$(curl -s -d '{
         "action": "updateNoteFields",
@@ -61,7 +76,6 @@ if [ -f "$imgsrc" ]; then
             }
         }
     }' -H 'Content-Type: application/json' http://127.0.0.1:8765)
-
     echo "updateNoteFields result: $updateNoteFields"
 
     guiBrowse=$(curl -s -d '{
